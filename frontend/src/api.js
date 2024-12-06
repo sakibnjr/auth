fce8fd69740f334629203706ec0f6745ddc3b847
sakibnjr <sakibnjr@proton.me>
2024-12-06 11:16:26 +0600

    updated api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend URL
+  baseURL: "https://auth-1qap.onrender.com/api", // Backend URL
 });
 
 export default api;
